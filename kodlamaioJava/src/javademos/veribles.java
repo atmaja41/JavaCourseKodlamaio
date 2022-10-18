435e718c3e74033a474bea31b87b850c158cd2f2
coNNNti <m.atmaca.official@hotmail.com>
2022-10-18 19:49:04 +0300

    java

---
+++ kodlamaioJava/src/javademos/veribles.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javademos;
+
+/**
+ *
+ * @author matma
+ */
+public class veribles {
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12;
+        String metin = "Öğrenci Sayısı = ";
+        System.out.println(metin + ogrenciSayisi);
+        System.out.println(metin + ogrenciSayisi);
+        System.out.println(metin + ogrenciSayisi);
+    }
+
+}
