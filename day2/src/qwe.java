00f910464336560b14cac870ecbdaee21a91c23f
coNNNti <m.atmaca.official@hotmail.com>
2022-10-19 00:15:38 +0300

    java

---
+++ day2/src/qwe.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author matma
+ */
+public class qwe {
+    public static void main(String[] args) {
+        System.out.println("sdas");
+    }
+}
