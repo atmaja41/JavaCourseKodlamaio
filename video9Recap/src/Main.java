fc9f89a798ca10520372483d467eb6384e4c37ef
coNNNti <m.atmaca.official@hotmail.com>
2022-10-20 02:14:32 +0300

    java

--- video9Recap/src/Main.java
+++ video9Recap/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         int sayi1 = 24;
-        int sayi2 = 25;
+        int sayi2 = 24;
         int sayi3 = 2;
         int enBuyuk = sayi1;
         if (enBuyuk < sayi2) {
