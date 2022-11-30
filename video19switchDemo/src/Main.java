8dd323e73b9f83216fffb2b09ad49e99b6d54f18
coNNNti <m.atmaca.official@hotmail.com>
2022-11-30 21:45:43 +0300

    recapdemo1

---
+++ video19switchDemo/src/Main.java
@@ -0,0 +1,29 @@
+
+import javax.xml.transform.OutputKeys;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char grade = 'A';
+        switch (grade) {
+            case 'A':
+                System.out.println("Mükemmel geçtiniz.");
+                break;
+            case 'B':
+                System.out.println("Çok güzel geçtiniz.");
+                break;
+            case 'C':
+                System.out.println("İyi geçtiniz.");
+                break;
+            case 'D':
+                System.out.println("Fena değil geçtiniz.");
+                break;
+            case 'F':
+                System.out.println("Malesef kaldınız");
+                break;
+            default:
+                System.out.println("Geçersiz not girdiniz.");
+        }
+    }
+
+}
