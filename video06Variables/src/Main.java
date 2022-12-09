272731ad799a9ab04a89fa1d0e150df6f63068ee
coNNNti <m.atmaca.official@hotmail.com>
2022-12-09 20:24:23 +0300

    java

---
+++ video06Variables/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+//camel case
+//code reusability 
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 13;
+        String mesaj = "Öğrenci sayısı : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+        System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+    }
+
+}
