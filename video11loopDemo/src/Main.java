26cdba70f3720166a3e251c4dbed289fd646f1ba
coNNNti <m.atmaca.official@hotmail.com>
2022-12-11 17:08:15 +0300

    java

--- video11loopDemo/src/Main.java
+++ video11loopDemo/src/Main.java
@@ -10,10 +10,24 @@
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 2; i <= 10; i+=2) {
+        for (int i = 2; i <= 10; i += 2) {
             System.out.println(i);
         }
-        System.out.println("Döngü Bitti");
-    }
+        System.out.println("For Döngüsü Bitti");
+//While
+        int i = 2;
+        while (i < 10) {
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("Do-While Döngüsü Bitti");
+//Do-While
+int j = 2;//kod satır satır ilerlediği için şartı sağlamasa bile bir kere 
+          //çalışır.Şart sağlandığında normal while gibi çalışır.
+        do {
+            System.out.println(j);
+            j += 2;
+        } while (j < 10);
 
+    }
 }
