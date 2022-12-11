7a4cb52d180a11dc691e1a30eb0b9406f296c440
coNNNti <m.atmaca.official@hotmail.com>
2022-12-11 16:34:35 +0300

    java

--- video10switchDemo/src/main.java
+++ video10switchDemo/src/main.java
@@ -8,5 +8,27 @@
  * @author matma
  */
 public class main {
-    
+
+    public static void main(String[] args) {
+        char grade = 'B';
+        switch (grade) {
+            case 'A':
+                System.out.println("Mükemmel : Geçtiniz");
+                break;
+            case 'B':
+                System.out.println("Çok Güzel : Geçtiniz");
+                break;
+            case 'C':
+                System.out.println("İyi : Geçtiniz");
+                break;
+            case 'D':
+                System.out.println("Fena Değil : Geçitiniz");
+                break;
+            case 'F':
+                System.out.println("Malesef : Kaldınız");
+                break;
+            default:
+                System.out.println("Geçersiz not girdiniz.");
+        }
+    }
 }
